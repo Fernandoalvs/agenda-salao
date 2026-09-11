87d48664664f61a835b319245c1c8aaa21253348
Fernandoalvs <serv.fernandoalves@gmail.com>
2026-09-11 12:23:04 -0300

    Add files via upload

--- app_salao.py
+++ app_salao.py
@@ -4,7 +4,6 @@ import os
 from datetime import datetime
 
 ARQUIVO_DADOS = "agenda_salao.json"
-
 SENHA_SALAO = "1234"
 
 
@@ -31,30 +30,25 @@ def salvar_dados(agendamentos):
         json.dump(agendamentos, arquivo, indent=4, ensure_ascii=False)
 
 
-
 agendamentos, proximo_id = carregar_dados()
-
 
 st.set_page_config(page_title="Agenda do Salão", page_icon="💇‍♀️", layout="centered")
 
 st.title("💇‍♀️ Agendamento Online - Studio Jeh Beaute")
 st.write("Escolha o seu serviço e reserve o seu horário de forma rápida!")
 
-
 aba_cliente, aba_salao = st.tabs(["➕ Marcar Horário", "📋 Visualizar Agenda (Salão)"])
-
 
 with aba_cliente:
     st.header("Faça o seu agendamento")
 
-    if'mensagem_sucesso' in st.session_state:
-        st.success(st.session_state['mensagem_sucesso'])
-        del st.session_state['mensagem_sucesso']
+    if "mensagem_sucesso" in st.session_state:
+        st.success(st.session_state["mensagem_sucesso"])
+        del st.session_state["mensagem_sucesso"]
 
     nome = st.text_input("Qual o seu nome?")
     servico = st.selectbox("Escolha o serviço:",
-                           ["Corte Feminino", "Manicure", "Pedicure", "Escova/Escova Progressiva"])
-
+                           ["Corte Feminino", "Corte Masculino", "Manicure", "Pedicure", "Escova/Escova Progressiva"])
 
     data = st.date_input("Escolha o dia:", min_value=datetime.today(), format="DD/MM/YYYY")
     horario = st.time_input("Escolha o horário:")
@@ -68,21 +62,19 @@ with aba_cliente:
                 "horario": str(horario.strftime('%H:%M'))
             }
             salvar_dados(agendamentos)
-            st.success(f"🎉 Perfeito, {nome}! Seu horário para {servico} foi reservado com sucesso!")
+
+            st.session_state[
+                "mensagem_sucesso"] = f"🎉 Perfeito, {nome}! Seu horário para {servico} em {data.strftime('%d/%m/%Y')} às {horario.strftime('%H:%M')} foi reservado!"
             st.rerun()
         else:
             st.error("⚠️ Por favor, digite o seu nome antes de confirmar.")
 
-
 with aba_salao:
     st.header("Área Restrita do Salão")
 
-
     senha_digitada = st.text_input("Digite a senha do salão para acessar a agenda:", type="password")
 
-
     if senha_digitada == SENHA_SALAO:
-        st.success("🔓 Acesso liberado!")
         st.write("---")
 
         if not agendamentos:
@@ -98,5 +90,4 @@ with aba_salao:
                         salvar_dados(agendamentos)
                         st.rerun()
     elif senha_digitada != "":
-
         st.error("⚠️ Senha incorreta! Acesso negado.")
